81aebfa9f91921aab1f6f9fe849a359f8ead9f96
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-06-28 15:33:10 -0300

    MongoDB connection created

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
+const app = express();
 
 app.use(express.json());
 app.use(helmet());
@@ -35,3 +36,12 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+//DATABASE
+const PORT = process.env.PORT || 5001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server on port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
